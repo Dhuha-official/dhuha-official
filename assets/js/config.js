c958ce16f23bc602f45dffe3379f12f5747573c4
Dhuha-official <gecrave.id@gmail.com>
2026-07-13 03:10:52 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://yjddkxffzqowrkkoqwth.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://yjddkxffzqowrkkoqwth.supabase.co";
 const SUPABASE_KEY = "sb_publishable_hid0DW65Zkt-tLC4o56S9g_JXt4_uCK";
 
 const supabaseClient = supabase.createClient(
