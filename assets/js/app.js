3ce80a1eaabff98cfaaa8078732a6b1e45963f4d
Dhuha-official <gecrave.id@gmail.com>
2026-07-17 23:04:54 +0700

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,1 +0,0 @@
-// DHUHA Official
--- assets/js/config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://yjddkxffzqowrkkoqwth.supabase.co";
-const SUPABASE_KEY = "sb_publishable_hid0DW65Zkt-tLC4o56S9g_JXt4_uCK";
-
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
